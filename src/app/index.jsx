0c6baeb11088950d59575708ca2f7c63eb37c4c3
Mario Espinoza <mario.switch100@gmail.com>
2026-09-24 12:11:10 -0400

    created an header of the page. as well import the font from google-fonts

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,14 +1,35 @@
-import { StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-
-
-
+import {
+  Manrope_400Regular,
+  Manrope_800ExtraBold,
+  useFonts,
+} from "@expo-google-fonts/manrope";
 
 export default function HomeScreen() {
+  const [fontsLoaded] = useFonts({
+    Manrope_400Regular,
+    Manrope_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-
+      <View style={styles.content}>
+        <View style={styles.header}>
+          <View>
+            <Text style={styles.title}>My Tasks</Text>
+            <Text style={styles.subtitle}>4 tasks remaining</Text>
+          </View>
+          <Pressable style={styles.addButton}>
+            <Text style={styles.addButtontext}>+</Text>
+          </Pressable>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -16,8 +37,44 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
+    justifyContent: "center",
+    flexDirection: "row",
+    color: "black",
+    padding: 20,
   },
-  
+  content: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 60,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontFamily: "Manrope_800ExtraBold",
+    color: "#1C1C1E",
+  },
+  subtitle: {
+    fontSize: 14,
+    fontFamily: "Manrope_400Regular",
+    color: "#8E8E93",
+  },
+  addButton: {
+    width: 48,
+    height: 48,
+    backgroundColor: "#3478F6",
+    borderRadius: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addButtontext: {
+    color: "white",
+    fontSize: 32,
+    marginTop: -6,
+    borderRadius: 2,
+  },
 });
